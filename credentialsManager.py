2f83b4b11ed253fe744725499cbaa9cda5ab7b58
Caleb Tony <calebmbugua@gmail.com>
2021-04-12 09:49:44 +0300

    changes fileedit to file manager and added some staticmethods and change passwordGenerator to passwordGeneratorEngine

--- credentialsManager.py
+++ credentialsManager.py
@@ -2,25 +2,44 @@ import fileManager
 import passwordGeneratorEngine
 
 class Credentials:
-        
-    def __init__(self, email, password):
-            self.isAuthenticated = false
+    def __init__(self, email):
+            self.isAuthenticated = False
             self.email = email
-            self.password = password
             self.savedPasswords = []
             
+    @staticmethod
+    def generateVerifiedPassword(pass_length):
+       return passwordGeneratorEngine.generateNewPassword(pass_length)
             
-    def loadPasswords():
-        res = fileEditor.getSavedPasswordItems() 
+    # @staticmethod
+    def loadPasswords(self):
+        res = fileManager.getSavedPasswordItems(self.email, self.isAuthenticated) 
         if(res["status"] == "success"):
             self.savedPasswords = res.data
         print(res["message"])
+      
+      
+    @staticmethod  
+    def createNewCredentialItem(newCredentialName, newCredentialUser, passwordLength, email):
         
-    def createNewCredentialItem(newCredentialName, newCredentialUser, passwordLength):
-        newPassword = passwordGenerator.generateNewPassword(passwordLength)
-        res = fileEditor.addNewPasswordItem(credentialName, credentialUser, newPassword, self.isAuthenticated)
+        newResponse = dict()
+        
+        newPassword = passwordGeneratorEngine.generateNewPassword(passwordLength)
+        res = fileManager.addNewPasswordItem(newCredentialName, newCredentialUser, newPassword, email)
+        
+        if res["status"] == "success":
+            newResponse["status"] = "success"
+            newResponse["data"] = newPassword
+            return newResponse
+            
+        else:
+            newResponse["status"] = "failed"
+            return newResponse
+        
         print(res["message"])
         
-    def addExistingCredentialItem(newCredentialName, newCredentialUser, newCredentialPassword):        
-        res =  fileEditor.addNewPasswordItem(credentialName, credentialUser, credentialPassword, self.isAuthenticated)
+    
+    @staticmethod
+    def addExistingCredentialItem(newCredentialName, newCredentialUser, newCredentialPassword, userEmail):     
+        res =  fileManager.addNewPasswordItem(newCredentialName, newCredentialUser, newCredentialPassword, userEmail)
         print(res["message"])
