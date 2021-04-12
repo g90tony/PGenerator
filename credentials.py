c86445cce64172ae094e20791e07f37a9d3f64a5
Caleb Tony <calebmbugua@gmail.com>
2021-04-12 10:34:07 +0300

    created a credintial class to keep track of the credentials

---
+++ credentials.py
@@ -0,0 +1,20 @@
+class Credentials:
+    def __init__(self):
+        self.accountID = None
+        self.accountUsername = None
+        self.accountPassword = None
+          
+    def createCredential(self, id, username, password):
+        self.accountID = id
+        self.accountUsername = username
+        self.accountPassword = password
+            
+    def loadCredential():
+        credentialInstance = dict()
+        
+        credentialInstance["id"] = self.id
+        credentialInstance["username"] = self.accountUsername
+        credentialInstance["savedPassword"] = self.accountPassword
+        
+    def updatePassword(password):
+        self.accountPassword = password
