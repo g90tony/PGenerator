56e8fc11a441a603785b8bbb86495e2ffcd3ed94
Caleb Tony <calebmbugua@gmail.com>
2021-04-12 14:00:36 +0300

    completed debugging works well

--- authenticator.py
+++ authenticator.py
@@ -1,19 +1,19 @@
 from os import path
 
-def loginUser(self):
+def loginUser(userEmail, userPassword):
     newResponse = dict()
     try:
         with open("user_authentication.txt", "r+") as authDB:
-            requestCredentials = "{}#{}\n".format(email, password)
+            requestCredentials = "{}#{}\n".format(userEmail, userPassword)
             for existingUser in authDB : 
-               
+                print("checking logged user:",existingUser)
+                
                 if(requestCredentials == existingUser):                   
                     newResponse["status"] = "success"
                     return newResponse
                 
-                else:
-                    newResponse["status"] = "failed"
-                    return newResponse
+            newResponse["status"] = "failed"
+            return newResponse
                 
     except Exception as e:
         print(str(e))
@@ -29,15 +29,11 @@ def registerNewUser(email, password, password_2):
         try:
             with open("user_authentication.txt", "a") as auth_DB:
                 data = auth_DB.write("{}#{}\n".format(email, password))
-                newResponse["status"] = "success"
-                return newResponse
-            
+           
             
             if (path.exists("./user_files/{}.txt".format(email))):
                 newResponse["status"] = "failed"
                 raise Exception("Please use another email address. That one already exists")
-            
-                return newResponse
     
             else: 
                 with open("./user_files/{}.txt".format(email), "w+") as newTable:
