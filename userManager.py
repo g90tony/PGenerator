61cb4b02eff1b98fd8c20a655d4f6d7a396b11a0
Caleb Tony <calebmbugua@gmail.com>
2021-04-12 10:24:33 +0300

    added setter and getter for savedPasswords

--- userManager.py
+++ userManager.py
@@ -4,6 +4,7 @@ class User:
     def __init__(self):
         self.email = None
         self.password = None
+        self.savedPasswords = list()
     
     newResponse = dict()          
                 
@@ -11,5 +12,11 @@ class User:
         self.email = email
         self.password = password
         
+    def loadSavedPasswords(passwords):
+        self.savedPasswords = passwords
+        
     def getUserEmail:
         userData["email"] = self.email
+        
+    def getSavedPassword:
+        userData["passwords"] = self.savedPasswords
